4009ff953dbf2a062adb4419869a77bab574140a
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-02 21:05:56 +0530

    refactor(App): Abstracted routes

--- src/app.js
+++ src/app.js
@@ -1,46 +1,18 @@
-// const app = require("fastify")();
 import fastify from "fastify";
-import Spotify from "./Helpers/spotify.js";
-// const Spotify = require("./Helpers/spotify.js");
+import FlatMetadata from "./routes/Flat.js";
+import FullMetadata from "./routes/Full.js";
 
 const app = fastify();
 
-app.get("/", async (req, res) => {
-  res.send({ hello: "world" });
-});
+app.register(FlatMetadata);
+app.register(FullMetadata);
 
-app.get("/flat/:type/:uri", async (req, res) => {
-  const uri = req.params.uri;
-  const type = req.params.type;
-  const spotify = new Spotify(uri, type);
-
-  let response;
-
-  if (type === "playlist") response = await spotify.getFlattenedPlaylist();
-  else if (type === "track") response = await spotify.getFlattenedData();
-
-  res.send(response);
-});
-
-app.get("/full/:type/:uri", async (req, res) => {
-  const uri = req.params.uri;
-  const type = req.params.type;
-  const spotify = new Spotify(uri, type);
-
-  let response;
-
-  if (type === "playlist") response = await spotify.parsePlaylist();
-  else if (type === "track") response = await spotify.getMetaData();
-
-  res.send(response);
-});
-
-app.listen(1232, (err, addr) => {
+app.listen({ port: 1232 }, function (err, address) {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`server listening on ${addr}`);
+  // Server is now listening on ${address}
 });
 
 export default app;
