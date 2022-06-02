a72c9ec3e0165baa610dbd92feabfb18c868aee5
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-02 21:06:21 +0530

    refactor(Route): Abstracted route

---
+++ src/routes/Full.js
@@ -0,0 +1,15 @@
+import Spotify from "../Helpers/spotify.js";
+
+export default async function FullMetadata(app, options) {
+  app.get("/full/:type/:uri", async (req, res) => {
+    const uri = req.params.uri;
+    const type = req.params.type;
+    const spotify = new Spotify(uri, type);
+    let response;
+
+    if (type === "playlist") response = await spotify.parsePlaylist();
+    else if (type === "track") response = await spotify.getMetaData();
+
+    res.send(response);
+  });
+}
