cfdeb664970d02c113c13787189037d34a43f920
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-04 00:22:54 +0530

    feat(App): Register search route

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import fastify from "fastify";
 import FlatMetadata from "./routes/Flat.js";
 import FullMetadata from "./routes/Full.js";
+import Search from "./routes/Search.js";
 
 const app = fastify();
 
 app.register(FlatMetadata);
 app.register(FullMetadata);
+app.register(Search);
 
 app.listen({ port: 1232 }, function (err, address) {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  // Server is now listening on ${address}
 });
 
 export default app;
