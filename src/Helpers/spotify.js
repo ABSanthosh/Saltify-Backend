95da0d969d2d55d7037b241058189d056aa197b5
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-05 01:22:30 +0530

    refactor(JS): ESM to CJS for Deta

--- src/Helpers/spotify.js
+++ src/Helpers/spotify.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 class Spotify {
   constructor(URI, type) {
@@ -53,4 +53,4 @@ class Spotify {
   }
 }
 
-export default Spotify;
+module.exports = Spotify;
